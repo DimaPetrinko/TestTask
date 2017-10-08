2eccea055a998fe424b2ff259012760eb333b7d8
Blind56 <petrinko6@gmail.com>
2017-10-08 15:46:34 +0300

    IS IT REALLY DONE?

--- Assets/Scripts/LocalGameManager.cs
+++ Assets/Scripts/LocalGameManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UniRx;
 
 public class LocalGameManager : MonoBehaviour   //this manager is local to every scene. it holds instances of important scripts
 {
@@ -12,7 +14,8 @@ public class LocalGameManager : MonoBehaviour   //this manager is local to every
     public MeshObjectController meshInstanceController;
 
     private GameData gameData;
-    private int timeElapsed = 0;
+    //private int timeElapsed = 0;
+    public IntReactiveProperty TimeElapsed = new IntReactiveProperty();
 
     private void OnEnable()
     {
@@ -29,35 +32,69 @@ public class LocalGameManager : MonoBehaviour   //this manager is local to every
         StartCoroutine(LoadDataFromRecources());
     }
 
+    private void Start()
+    {
+        TimeElapsed.SubscribeToText(uiController.counter);
+
+        //int y = ((int c)  => c++);
+    }
+
+    CompositeDisposable disposables = new CompositeDisposable(); // field
+
     private void OnClick(Vector3 position, int layer)
     {
-        StartCoroutine(TickinBomb());
+        StartTimer();
+        //Observable.
+        //StartCoroutine(TickinBomb());
+    }
+
+    private void StartTimer()
+    {
+        disposables.Clear();
+        Debug.Log("Subscribing..");
+            TimeElapsed.Value = 0;
+        Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x => ProcessTick(x)).AddTo(disposables);
+    }
+
+    private void ProcessTick(float seconds)
+    {
+        seconds++;
+        Debug.Log("Tick");
+        TimeElapsed.Value = (int)seconds;
+        if (seconds >= gameData.timeTillUpdate)
+        {
+            UpdateColor();
+            StartTimer();
+        }
+        //else
+        //{
+        //}
     }
 
     private void UpdateColor()
     {
-        Color randomColor = new Color(Random.value, Random.value, Random.value);
+        Color randomColor = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
         meshInstanceController.UpdateColor(randomColor);
     }
 
-    private IEnumerator Tick()
-    {
-        uiController.UpdateCounter(timeElapsed + "");
-        yield return new WaitForSeconds(1f);
-        timeElapsed++;
-        StopCoroutine(Tick());
-        StartCoroutine(Tick());
-    }
+    //private IEnumerator Tick()
+    //{
+    //    uiController.UpdateCounter(timeElapsed + "");
+    //    yield return new WaitForSeconds(1f);
+    //    timeElapsed++;
+    //    StopCoroutine(Tick());
+    //    StartCoroutine(Tick());
+    //}
 
-    private IEnumerator TickinBomb()
-    {
-        timeElapsed = 0;
-        StartCoroutine(Tick());
-        yield return new WaitForSeconds(gameData.timeTillUpdate);
-        UpdateColor();
-        StopAllCoroutines();
-        StartCoroutine(TickinBomb());
-    }
+    //private IEnumerator TickinBomb()
+    //{
+    //    timeElapsed = 0;
+    //    StartCoroutine(Tick());
+    //    yield return new WaitForSeconds(gameData.timeTillUpdate);
+    //    UpdateColor();
+    //    StopAllCoroutines();
+    //    StartCoroutine(TickinBomb());
+    //}
 
     private IEnumerator LoadDataFromRecources()
     {
